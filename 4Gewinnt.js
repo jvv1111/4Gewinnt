98ba6048494f2677748559892572aeaca412fb7a
Malik-Arrak <93769996+Malik-Arrak@users.noreply.github.com>
2023-06-19 15:35:35 +0200

    Update 4Gewinnt.js
    you are able to win now BOOOOMMMMMNM

--- 4Gewinnt.js
+++ 4Gewinnt.js
@@ -1,7 +1,9 @@
-const spielfeld = ["arr0", "arr1", "arr2", "arr3", "arr4", "arr5", "arr6", "arr7", "arr8", "arr9", "arr10", "arr11", "arr12", "arr13", "arr14", "arr15", "arr16", "arr17", "arr18", "arr19", "arr20",
-    "arr21", "arr22", "arr23", "arr24", "arr25", "arr26", "arr27", "arr28", "arr29", "arr30", "arr31", "arr32", "arr33", "arr34", "arr35", "arr36", "arr37", "arr38", "arr39", "arr40", "arr41"];
+const spielfeld = ["arr0", "arr1", "arr2", "arr3", "arr4", "arr5", "arr6", "arr7", "arr8", "arr9", "arr10", "arr11", "arr12", "arr13",
+    "arr14", "arr15", "arr16", "arr17", "arr18", "arr19", "arr20", "arr21", "arr22", "arr23", "arr24", "arr25", "arr26", "arr27", "arr28",
+    "arr29", "arr30", "arr31", "arr32", "arr33", "arr34", "arr35", "arr36", "arr37", "arr38", "arr39", "arr40", "arr41"];
 
 function freierIndex(spielfeld, index) {
+
     for (let i = index; i <= index + 5; i++) {
         const id = spielfeld[i];
         let element = document.getElementById(id);
@@ -9,18 +11,14 @@ function freierIndex(spielfeld, index) {
         if (src === "kreis_nichts.png") {
             return i;
         }
-    }
-    return -1;
+    } return -1;
 }
 
 const dreieck1 = document.getElementById("dreieck1");
 dreieck1.addEventListener("click", () => setzeSpalte(1));
 
 const dreieck2 = document.getElementById("dreieck2");
-dreieck2.addEventListener("click", function () {
-    setzeSpalte(2);
-
-});
+dreieck2.addEventListener("click", function () { setzeSpalte(2); });
 
 document.getElementById("dreieck3").addEventListener("click", () => setzeSpalte(3));
 
@@ -32,23 +30,116 @@ document.getElementById("dreieck6").addEventListener("click", () => setzeSpalte(
 
 document.getElementById("dreieck7").addEventListener("click", () => setzeSpalte(7));
 
-function setzeSpalte(spalte) {
-    const index = (spalte - 1) * 6;
 
-    // 1. index berechnen
-    // 2. int index = (eingabe - 1)  * 6;
-    // 3. code block mit index ausführen
+const spieler = document.getElementById("spieler");
+
+function setzeSpalte(spalte) {
+
+    const index = (spalte - 1) * 6;
+    // 1. index berechnen 
+    // 2. int index = (eingabe - 1) * 6; 
+    // 3. code block mit index ausführen 
 
     const frei = freierIndex(spielfeld, index);
-    //if (frei == -1)
-    //    System.out.println("Spalte voll neue eingabe");
+    //if (frei == -1) 
 
     const element = document.getElementById(spielfeld[frei]);
-    element.setAttribute(`src`, `kreis_gelb.png`);
 
-    const spieler = document.getElementById("spieler");
-    spieler.textContent = "Spieler 2 ist dran";
+    if (spieler.textContent === "Spieler 1 ist dran") {
+        element.setAttribute(`src`, `kreis_rot.png`);
+        if (gewinnMöglichkeiten(spielfeld, "kreis_rot.png")) {
+            spieler.textContent = "SP1 GEWONNEN";
+            return;
+        }
+        spieler.textContent = "Spieler 2 ist dran";
+    } else if (spieler.textContent === "Spieler 2 ist dran") {
+        element.setAttribute(`src`, `kreis_gelb.png`);
+        if (gewinnMöglichkeiten(spielfeld, `kreis_gelb.png`)) {
+            spieler.textContent = "SP2 GEWONNEN";
+            return;
+        }
+        spieler.textContent = "Spieler 1 ist dran";
+    }
+
+    if (freierIndex(spielfeld, index) === -1) {
+        const dreieckId = "dreieck" + spalte;
+        const dreieck = document.getElementById(dreieckId);
+        dreieck.classList.add("spalte-deaktiviert");
+    }
 }
 
+function gewinnMöglichkeiten(spielfeld, spielerBild) {
 
+    /*Gewinner Vertikal*/
+    for (let i = 0; i <= 36; i = i + 6) {
+        if ((hatSpielerFeldBelegt(i, spielerBild) && hatSpielerFeldBelegt(i + 1, spielerBild) && hatSpielerFeldBelegt(i + 2, spielerBild) && hatSpielerFeldBelegt(i + 3, spielerBild))
+            || (hatSpielerFeldBelegt(i + 1, spielerBild) && hatSpielerFeldBelegt(i + 2, spielerBild) && hatSpielerFeldBelegt(i + 3, spielerBild) && hatSpielerFeldBelegt(i + 4, spielerBild))
+            || (hatSpielerFeldBelegt(i + 2, spielerBild) && hatSpielerFeldBelegt(i + 3, spielerBild) && hatSpielerFeldBelegt(i + 4, spielerBild) && hatSpielerFeldBelegt(i + 5, spielerBild))) {
+            return true;
+        }
+    }
 
+    /*gewinnerWagerecht*/
+    for (let i = 0; i <= 23; i++) {
+        if (hatSpielerFeldBelegt(i, spielerBild) && hatSpielerFeldBelegt(i + 6, spielerBild) &&
+            hatSpielerFeldBelegt(i + 12, spielerBild) && hatSpielerFeldBelegt(i + 18, spielerBild)) {
+            return true;
+        }
+    }
+
+    /*gewinnerDiagonal*/
+    if (hatSpielerFeldBelegt(0, spielerBild) && hatSpielerFeldBelegt(7, spielerBild) && hatSpielerFeldBelegt(14, spielerBild) && hatSpielerFeldBelegt(21, spielerBild)) {
+        return true;
+    } else if (hatSpielerFeldBelegt(7, spielerBild) && hatSpielerFeldBelegt(14, spielerBild) && hatSpielerFeldBelegt(21, spielerBild) && hatSpielerFeldBelegt(18, spielerBild)) {
+        return true;
+    } else if (hatSpielerFeldBelegt(14, spielerBild) && hatSpielerFeldBelegt(21, spielerBild) && hatSpielerFeldBelegt(28, spielerBild) && hatSpielerFeldBelegt(35, spielerBild)) {
+        return true;
+    } else if (hatSpielerFeldBelegt(1, spielerBild) && hatSpielerFeldBelegt(8, spielerBild) && hatSpielerFeldBelegt(15, spielerBild) && hatSpielerFeldBelegt(22, spielerBild)) {
+        return true;
+    } else if (hatSpielerFeldBelegt(8, spielerBild) && hatSpielerFeldBelegt(15, spielerBild) && hatSpielerFeldBelegt(22, spielerBild) && hatSpielerFeldBelegt(29, spielerBild)) {
+        return true;
+    } else if (hatSpielerFeldBelegt(2, spielerBild) && hatSpielerFeldBelegt(9, spielerBild) && hatSpielerFeldBelegt(16, spielerBild) && hatSpielerFeldBelegt(23, spielerBild)) {
+        return true;
+    } else if (hatSpielerFeldBelegt(6, spielerBild) && hatSpielerFeldBelegt(13, spielerBild) && hatSpielerFeldBelegt(20, spielerBild) && hatSpielerFeldBelegt(27, spielerBild)) {
+        return true;
+    } else if (hatSpielerFeldBelegt(13, spielerBild) && hatSpielerFeldBelegt(20, spielerBild) && hatSpielerFeldBelegt(27, spielerBild) && hatSpielerFeldBelegt(34, spielerBild)) {
+        return true;
+    } else if (hatSpielerFeldBelegt(20, spielerBild) && hatSpielerFeldBelegt(27, spielerBild) && hatSpielerFeldBelegt(34, spielerBild) && hatSpielerFeldBelegt(41, spielerBild)) {
+        return true;
+    } else if (hatSpielerFeldBelegt(12, spielerBild) && hatSpielerFeldBelegt(19, spielerBild) && hatSpielerFeldBelegt(26, spielerBild) && hatSpielerFeldBelegt(33, spielerBild)) {
+        return true;
+    } else if (hatSpielerFeldBelegt(19, spielerBild) && hatSpielerFeldBelegt(26, spielerBild) && hatSpielerFeldBelegt(33, spielerBild) && hatSpielerFeldBelegt(40, spielerBild)) {
+        return true;
+    } else if (hatSpielerFeldBelegt(18, spielerBild) && hatSpielerFeldBelegt(25, spielerBild) && hatSpielerFeldBelegt(32, spielerBild) && hatSpielerFeldBelegt(39, spielerBild)) {
+        return true;
+    } else if (hatSpielerFeldBelegt(38, spielerBild) && hatSpielerFeldBelegt(33, spielerBild) && hatSpielerFeldBelegt(28, spielerBild) && hatSpielerFeldBelegt(23, spielerBild)) {
+        return true;
+    } else if (hatSpielerFeldBelegt(37, spielerBild) && hatSpielerFeldBelegt(32, spielerBild) && hatSpielerFeldBelegt(27, spielerBild) && hatSpielerFeldBelegt(22, spielerBild)) {
+        return true;
+    } else if (hatSpielerFeldBelegt(36, spielerBild) && hatSpielerFeldBelegt(31, spielerBild) && hatSpielerFeldBelegt(26, spielerBild) && hatSpielerFeldBelegt(21, spielerBild)) {
+        return true;
+    } else if (hatSpielerFeldBelegt(32, spielerBild) && hatSpielerFeldBelegt(27, spielerBild) && hatSpielerFeldBelegt(22, spielerBild) && hatSpielerFeldBelegt(17, spielerBild)) {
+        return true;
+    } else if (hatSpielerFeldBelegt(31, spielerBild) && hatSpielerFeldBelegt(26, spielerBild) && hatSpielerFeldBelegt(21, spielerBild) && hatSpielerFeldBelegt(16, spielerBild)) {
+        return true;
+    } else if (hatSpielerFeldBelegt(30, spielerBild) && hatSpielerFeldBelegt(25, spielerBild) && hatSpielerFeldBelegt(20, spielerBild) && hatSpielerFeldBelegt(15, spielerBild)) {
+        return true;
+    } else if (hatSpielerFeldBelegt(26, spielerBild) && hatSpielerFeldBelegt(21, spielerBild) && hatSpielerFeldBelegt(16, spielerBild) && hatSpielerFeldBelegt(11, spielerBild)) {
+        return true;
+    } else if (hatSpielerFeldBelegt(25, spielerBild) && hatSpielerFeldBelegt(20, spielerBild) && hatSpielerFeldBelegt(15, spielerBild) && hatSpielerFeldBelegt(10, spielerBild)) {
+        return true;
+    } else if (hatSpielerFeldBelegt(24, spielerBild) && hatSpielerFeldBelegt(19, spielerBild) && hatSpielerFeldBelegt(14, spielerBild) && hatSpielerFeldBelegt(9, spielerBild)) {
+        return true;
+    } else if (hatSpielerFeldBelegt(20, spielerBild) && hatSpielerFeldBelegt(15, spielerBild) && hatSpielerFeldBelegt(10, spielerBild) && hatSpielerFeldBelegt(5, spielerBild)) {
+        return true;
+    } else if (hatSpielerFeldBelegt(19, spielerBild) && hatSpielerFeldBelegt(14, spielerBild) && hatSpielerFeldBelegt(9, spielerBild) && hatSpielerFeldBelegt(4, spielerBild)) {
+        return true;
+    } else if (hatSpielerFeldBelegt(18, spielerBild) && hatSpielerFeldBelegt(13, spielerBild) && hatSpielerFeldBelegt(8, spielerBild) && hatSpielerFeldBelegt(3, spielerBild)) {
+        return true;
+    }
+    return false;
+}
+
+function hatSpielerFeldBelegt(index, spielerBild) {
+    return document.getElementById(spielfeld[index]).getAttribute("src") === spielerBild;
+}
